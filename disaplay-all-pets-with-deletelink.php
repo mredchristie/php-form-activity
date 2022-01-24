3211daf9464821a84d81c1b1f4ecd75904ffc581
mredchristie <mredchristie@yahoo.com>
2022-01-24 22:08:53 +0000

    Update disaplay-all-pets-with-deletelink.php

---
+++ disaplay-all-pets-with-deletelink.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Display Pets Sctored in the Pets MySQL Table </title>
+
+</head>
+<body>
+    
+
+
+
+
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "task6";
+
+
+?>
+
+<?php
+echo "<table style='border: solid 1px black;'>";
+ echo "<tr><th>Pet Name</th><th>Pet Age</th><th>Pet Type</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT pet_name, pet_age, pet_type FROM pets");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
+
+
+
+
+
+
+
+</body>
+</html>
